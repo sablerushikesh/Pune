7922831b2768127f0fa916312b4a54a062b246d1
rushi <rushi@DESKTOP-KASF1I7>
2024-12-27 05:34:10 -0800

    Rushikesh

---
+++ Java/src/com/cjc/Test.java
@@ -0,0 +1,19 @@
+package com.cjc;
+
+public class Test {
+
+	public void java()
+	{
+		System.out.println("is best language in java");
+	}
+	public void paython()
+	{
+		System.out.println("the best programming language of paython");
+	}
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.java();
+		t.paython();
+	}
+}
